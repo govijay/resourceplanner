121f15b92bfacf5f783c5f44f8a5be8b1d6a2f13
Vijay <vijayg17@icloud.com>
2019-05-03 15:52:34 -0700

    initial commit

---
+++ ResourcePlanner/src/main/java/com/sap/ariba/cts/ResourcePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.sap.ariba.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcePlannerApplication.class, args);
+	}
+
+}

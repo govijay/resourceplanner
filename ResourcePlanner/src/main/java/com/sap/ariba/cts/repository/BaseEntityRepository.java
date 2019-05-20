5ca6f9bd05d6108e596ebbd1be2cb443c0a7279f
Vijay <vijayg17@icloud.com>
2019-05-20 15:08:33 -0700

    Master Data (Region, Department) -v1

---
+++ ResourcePlanner/src/main/java/com/sap/ariba/cts/repository/BaseEntityRepository.java
@@ -0,0 +1,26 @@
+package com.sap.ariba.cts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * The interface Base entity repository.
+ *
+ * @param <T>      the type parameter
+ * @param <String> the type parameter
+ * @author Vijay.G
+ * @version $Id : $
+ */
+@NoRepositoryBean
+public interface BaseEntityRepository<T, String extends Serializable> extends JpaRepository<T, String> {
+
+  /**
+   * Gets active entity.
+   *
+   * @return the active entity
+   */
+  List<T> getActiveEntity();
+}
